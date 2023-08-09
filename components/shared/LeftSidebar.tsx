3fe183d48018fd2198765b4c3a6659d095437f51
Stardust <stardustdev@outlook.com>
2023-08-09 13:14:41 -0500

    Add profile page

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,13 +3,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
-import { SignOutButton, SignedIn } from '@clerk/nextjs';
+import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
 
 import { sidebarLinks } from '@/constants';
 
 const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -18,6 +19,10 @@ const LeftSidebar = () => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+          if (link.route === '/profile') {
+            link.route = `/profile/${userId}`;
+          }
 
           return (
             <Link
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 
 import User from '../models/user.model';
 import Community from '../models/community.model';
+import Thread from '../models/thread.model';
 
 import { connectToDB } from '../mongoose';
 
@@ -56,4 +57,36 @@ export async function fetchUser(userId: string) {
       model: Community,
     });
   } catch (error) {}
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: 'threads',
+      model: Thread,
+      populate: [
+        {
+          path: 'community',
+          model: Community,
+          select: 'name id image _id', // Select the "name" and "_id" fields from the "Community" model
+        },
+        {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'name image id', // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error('Error fetching user threads:', error);
+    throw error;
+  }
 }
