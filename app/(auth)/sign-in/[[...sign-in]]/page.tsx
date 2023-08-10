f6babdba214fe82a24420d614ba68d425866e96e
Stardust <stardustdev@outlook.com>
2023-08-10 15:35:48 -0500

    Fix position of sign in modal

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="w-full h-[100vh] flex">
+      <div className="m-auto">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sing-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sing-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full h-[100vh] flex">
+      <div className="m-auto">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
